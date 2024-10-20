33d454f934a72ca6de49a6c859110936044bf37a
ARPIT SINGHAL <161847701+EnchantedRepoRuler@users.noreply.github.com>
2024-10-20 12:07:13 +0000

    function started

---
+++ functionsandmethods.js
@@ -0,0 +1,30 @@
+// function myFunction(){
+//     console.log("Hello World!");
+//     console.log("We are learning JS ;-)");
+// }
+
+// myFunction();
+
+// function newFunction(msg){
+//     console.log(msg);
+// }
+
+// newFunction("namaste duniya!");
+//function to calculate sum of two number
+
+// function sum(a, b){
+//     let s = a+b;
+//     return s;
+
+// }
+
+// let val = sum(5,6);
+// console.log(val);
+
+
+//arrow function
+
+const arrowSum = (a,b) => {
+    console.log(a+b);
+};
+
